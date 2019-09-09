96bcb1e9b121befed695e9cb0ad4f2a0ac4b5d71
roushc <roushc@umich.edu>
2019-09-09 15:42:45 -0400

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+import random
+
+print("Chris")
+one = random.randint(0,100)
+two = random.randint(0,100)
+print(one)
+print(two)
+total = one + two
+print("Sum = " + str(total))
+print("Average = " + str(total/2.0))
+
